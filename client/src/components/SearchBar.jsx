22fe16178061f1757d6f5c232195b634da98f162
Siyi Ji <sylviajsyy@gmail.com>
2026-03-06 00:29:06 -0600

    Add styling for searchbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import "../scss/SearchBar.scss";
+import "../scss/SearchBar.scss";
 
 const SearchBar = ({ onSearch }) => {
   const [filters, setFilters] = useState({
@@ -18,28 +18,29 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <div className="search-section">
-      <label>
-        Start Date
-        <input
-          type="date"
-          value={filters.start}
-          onChange={(e) =>
-            setFilters({ ...filters, start: e.target.value })
-          }
-        />
-      </label>
+      <div className="search-field">
+        <label>
+          Start Date
+          <input
+            type="date"
+            value={filters.start}
+            onChange={(e) =>
+              setFilters({ ...filters, start: e.target.value })
+            }
+          />
+        </label>
 
-      <label>
-        End Date
-        <input
-          type="date"
-          value={filters.end}
-          onChange={(e) =>
-            setFilters({ ...filters, end: e.target.value })
-          }
-        />
-      </label>
-
+        <label>
+          End Date
+          <input
+            type="date"
+            value={filters.end}
+            onChange={(e) =>
+              setFilters({ ...filters, end: e.target.value })
+            }
+          />
+        </label>
+      </div>
       <button onClick={handleSearch}>Search</button>
     </div>
   );
